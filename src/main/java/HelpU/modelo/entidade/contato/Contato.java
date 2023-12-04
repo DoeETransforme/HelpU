29a7167e505bf3d6101d9187c4a05016264a2059
Arthur Miranda do Carmo <tucadocarmo100578@gmail.com>
2023-12-04 16:29:46 -0300

    foi criada a classe contato

---
+++ src/main/java/HelpU/modelo/entidade/contato/Contato.java
@@ -0,0 +1,43 @@
+package HelpU.modelo.entidade.contato;
+
+import java.io.Serializable;
+ 
+public class Contato implements Serializable{
+
+	private static final long serialVersionUID = 8094576640471907006L;
+	private String email;
+	private String celular;
+	private int idcontato;
+	
+	
+	public Contato (String celular, String email, int idcontato) {
+		setCelular(celular);
+		setEmail(email);
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public int getIdcontato() {
+		return idcontato;
+	}
+
+	public void setIdcontato(int idcontato) {
+		this.idcontato = idcontato;
+	}
+}
