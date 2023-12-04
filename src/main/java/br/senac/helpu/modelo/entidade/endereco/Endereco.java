e6941e4687812bee0d87e170e54378bab9c519b6
Marlon Alex Andrade <marlonalexandrade@gmail.com>
2023-12-04 16:05:42 -0300

    Criada classe Endereço

---
+++ src/main/java/br/senac/helpu/modelo/entidade/endereco/Endereco.java
@@ -0,0 +1,116 @@
+package br.senac.helpu.modelo.entidade.endereco;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+	
+	private static final long serialVersionID = 1L;
+	
+	private Long idEndereco;
+	
+	private String logradouro;
+	
+	private String bairro;
+	
+	private String complemento;
+	
+	private int numero;
+	
+	private String cidade;
+	
+	private String unidadeFederativa;
+	
+	private String cep;
+	
+
+	public Endereco(Long idEndereco, String logradouro, String bairro, String complemento, int numero, String cidade,
+			String unidadeFederativa, String cep) {
+		super();
+		this.idEndereco = idEndereco;
+		this.logradouro = logradouro;
+		this.bairro = bairro;
+		this.complemento = complemento;
+		this.numero = numero;
+		this.cidade = cidade;
+		this.unidadeFederativa = unidadeFederativa;
+		this.cep = cep;
+	}
+
+	public Endereco(String logradouro, String bairro, String complemento, int numero, String cidade,
+			String unidadeFederativa, String cep) {
+		super();
+		this.logradouro = logradouro;
+		this.bairro = bairro;
+		this.complemento = complemento;
+		this.numero = numero;
+		this.cidade = cidade;
+		this.unidadeFederativa = unidadeFederativa;
+		this.cep = cep;
+	}
+
+	public Long getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Long idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUnidadeFederativa() {
+		return unidadeFederativa;
+	}
+
+	public void setUnidadeFederativa(String unidadeFederativa) {
+		this.unidadeFederativa = unidadeFederativa;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+}
