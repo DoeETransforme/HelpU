2153c44bc6b3aca67add041ad5009ac6f1ac7d35
Arthur Miranda do Carmo <tucadocarmo100578@gmail.com>
2023-12-07 16:37:32 -0300

    Criada classe Contato

--- src/main/java/br/senac/helpu/modelo/entidade/Contato/Contato.java
+++ src/main/java/br/senac/helpu/modelo/entidade/Contato/Contato.java
@@ -1,21 +1,53 @@
-package br.senac.helpu.modelo.entidade.Contato;
+package br.senac.helpu.modelo.entidade.contato;
 
 import java.io.Serializable;
- 
-public class Contato implements Serializable{
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import br.senac.helpu.modelo.entidade.usuario.Usuario;
+
+@Entity
+@Table(name = "contato")
+public class Contato implements Serializable {
+	
 	private static final long serialVersionUID = 8094576640471907006L;
-	private String email;
-	private String celular;
+	
+	@Id
+	@Column(name = "id_contato")
 	private Long id;
 	
+	@Column(name = "email_contato", length = 65, unique = true, nullable = false)
+	private String email;
 	
-	public Contato (String celular, String email, Long id) {
+	@Column(name = "celular_contato", length = 16, unique = true, nullable = false)
+	private String celular;
+
+	@OneToOne(fetch =FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+	
+	public Contato() {}
+	
+	public Contato(String celular, String email) {
 		setCelular(celular);
 		setEmail(email);
-		setId(id);
 	}
-	
+		
+	public Contato(Long id, String celular, String email) {
+		setId(id);
+		setCelular(celular);
+		setEmail(email);
+		
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
@@ -23,13 +55,15 @@ public class Contato implements Serializable{
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getCelular() {
 		return celular;
 	}
+
 	public void setCelular(String celular) {
 		this.celular = celular;
 	}
