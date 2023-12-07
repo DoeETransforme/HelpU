bd4eac1cd37eda57984d047d4ebed0b1335f7bce
Pedro Krubniki Albuquerque <pedro.krubniki@gmail.com>
2023-12-07 16:19:42 -0300

    Criado o mapeamento da classe Usuario

--- src/main/java/br/senac/helpu/modelo/entidade/usuario/Usuario.java
+++ src/main/java/br/senac/helpu/modelo/entidade/usuario/Usuario.java
@@ -2,24 +2,50 @@ package br.senac.helpu.modelo.entidade.usuario;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import br.senac.helpu.modelo.entidade.Contato.Contato;
 
+@Entity
+@Table(name = "usuario")
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = -8566282827144097443L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
 	private long id;
+	
+	@Column(name = "nome_usuario", length = 30, nullable = false, unique = true)
 	private String nome;
+	
+	@Column(name = "senha_usuario", length = 15, nullable = false)
 	private String senha;
+	
+	@Column(name = "contato_usuario", nullable = false)
 	private Contato contato;
-
+	
+	public Usuario() {}
+	
 	public Usuario(long id, String nome, String senha, Contato contato) {
 		setId(id);
 		setNome(nome);
 		setSenha(senha);
 		setContato(contato);
 	}
-
+	
+	public Usuario(String nome, String senha, Contato contato) {
+		setNome(nome);
+		setSenha(senha);
+		setContato(contato);
+	}
+	
 	public long getId() {
 		return id;
 	}
