f0bf57c63c61175e95b3769850427e1634e75672
Pedro Krubniki Albuquerque <pedro.krubniki@gmail.com>
2023-12-11 15:26:08 -0300

    Atualizada a classe Ong

--- src/main/java/br/senac/helpu/modelo/entidade/Ong/Ong.java
+++ src/main/java/br/senac/helpu/modelo/entidade/Ong/Ong.java
@@ -1,6 +1,7 @@
 package br.senac.helpu.modelo.entidade.Ong;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.senac.helpu.modelo.entidade.Contato.Contato;
@@ -10,15 +11,17 @@ import br.senac.helpu.modelo.entidade.usuario.Usuario;
 
 public class Ong extends Usuario implements Serializable {
 
+	private static final long serialVersionUID = -2680258071107532867L;
+
 	private String cnpj;
 	private Endereco endereco;
 	List<PedidoDoacao> pedidos;
 
 	public Ong(long id, String nome, String senha, Contato contato, String cnpj, Endereco endereco) {
 		super(id, nome, senha, contato);
-		this.cnpj = cnpj;
-		this.endereco = endereco;
-	
+		setCnpj(cnpj);
+		setEndereco(endereco);
+		pedidos = new ArrayList<>();
 	}
 
 	public String getCnpj() {
@@ -37,8 +40,12 @@ public class Ong extends Usuario implements Serializable {
 		this.endereco = endereco;
 	}
 
-	
-	
-	
-	
+	public void addPedido(PedidoDoacao pedido) {
+		pedidos.add(pedido);
+	}
+
+	public void removePedido(PedidoDoacao pedido) {
+		pedidos.remove(pedido);
+	}
+
 }
