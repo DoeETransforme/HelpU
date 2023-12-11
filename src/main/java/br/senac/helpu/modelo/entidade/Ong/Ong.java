7a8fb0d30b1ac10d83a92c3bb1ef4690247f47df
Pedro Krubniki Albuquerque <pedro.krubniki@gmail.com>
2023-12-11 15:23:54 -0300

    Atualizada a classe Ong

--- src/main/java/br/senac/helpu/modelo/entidade/Ong/Ong.java
+++ src/main/java/br/senac/helpu/modelo/entidade/Ong/Ong.java
@@ -3,47 +3,42 @@ package br.senac.helpu.modelo.entidade.Ong;
 import java.io.Serializable;
 import java.util.List;
 
+import br.senac.helpu.modelo.entidade.Contato.Contato;
 import br.senac.helpu.modelo.entidade.endereco.Endereco;
-import br.senac.helpu.modelo.enumeracao.statuspedido.StatusPedido;
+import br.senac.helpu.modelo.entidade.pedidodoacao.PedidoDoacao;
+import br.senac.helpu.modelo.entidade.usuario.Usuario;
 
-public class Ong implements Serializable{
+public class Ong extends Usuario implements Serializable {
 
-	private int id;
 	private String cnpj;
 	private Endereco endereco;
-	private List<StatusPedido> pedidodoacao;
-	private static final long serialVersionUID = -3707751312475379705L;
+	List<PedidoDoacao> pedidos;
+
+	public Ong(long id, String nome, String senha, Contato contato, String cnpj, Endereco endereco) {
+		super(id, nome, senha, contato);
+		this.cnpj = cnpj;
+		this.endereco = endereco;
 	
-	public Ong(int id, String cnpj, Endereco endereco, List<StatusPedido> pedidodoacao) {
-		setId(id);
-		setCnpj(cnpj);
-		setEndereco(endereco);
-		setPedidodoacao(pedidodoacao);
 	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public String getCnpj() {
 		return cnpj;
 	}
+
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
+
 	public Endereco getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-	public List<StatusPedido> getPedidodoacao() {
-		return pedidodoacao;
-	}
-	public void setPedidodoacao(List<StatusPedido> pedidodoacao) {
-		this.pedidodoacao = pedidodoacao;
-	}
 
+	
+	
+	
+	
 }
