2a4bc32576f8cefc81235e54347ee46e068e42c2
cauallr <caualucasllr@gmail.com>
2023-12-11 15:33:43 -0300

    Alterada a classe StatusProposta
    Foi realizado algumas correções de proposta doação

--- src/main/java/br/senac/helpu/modelo/entidade/pedidodoacao/PedidoDoacao.java
+++ src/main/java/br/senac/helpu/modelo/entidade/pedidodoacao/PedidoDoacao.java
@@ -4,47 +4,19 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import br.senac.helpu.modelo.entidade.Item.Item;
+import br.senac.helpu.modelo.entidade.item.Item;
 import br.senac.helpu.modelo.enumeracao.statuspedido.StatusPedido;
-
-@Entity
-@Table( name = "pedidoDoacao" )
 
 public class PedidoDoacao implements Serializable {
 
 	private static final long serialVersionUID = 5828763779674937613L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_pedido_Doacao")
+
 	private long id;
-	
-	@Column(name = "titulo", length = 25, nullable = false)
 	private String titulo;
-	
-	@Column(name = "descricao", length =150, nullable = true)
 	private String descricao;
-	
-	@Enumerated(EnumType.STRING)
-	@Column(name = "Status_Pedido", length = 50 , nullable = true)
 	private StatusPedido statusPedido;
-	
-	@OneToMany(fetch= FetchType.LAZY, mappedBy = "proposta_Doacao", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Item> itens;
 
-	
-	
 	public PedidoDoacao(long id, String titulo, String descricao, StatusPedido statusPedido) {
 		setId(id);
 		setTitulo(titulo);
