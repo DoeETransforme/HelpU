36ad5d762ff5bfda1870ff44375d645f11842e30
cauallr <caualucasllr@gmail.com>
2023-12-11 16:03:42 -0300

    alterado a classe item

--- src/main/java/br/senac/helpu/modelo/entidade/item/Item.java
+++ src/main/java/br/senac/helpu/modelo/entidade/item/Item.java
@@ -4,14 +4,33 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import br.senac.helpu.modelo.entidade.alimento.Alimento;
+@Entity
+@Table(name = "item")
 
 public class Item implements Serializable {
 
 	private static final long serialVersionUID = -4650927518777774993L;
 
+	private static final String Generationtype = null;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_item")
 	private Long id;
+	@Column(name = "quantidade_pedido", nullable = false)
 	private int quantidade;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "alimento", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Alimento> alimentos;
 
 	public Item(Long id, int quantidade) {
