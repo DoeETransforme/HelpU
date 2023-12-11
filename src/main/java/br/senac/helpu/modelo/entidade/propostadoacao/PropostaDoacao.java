63a59d0f2b805d0660af5ffd5eacb6f04ae0576c
cauallr <caualucasllr@gmail.com>
2023-12-11 15:48:48 -0300

    alterada a classe Proposta_Doacao

--- src/main/java/br/senac/helpu/modelo/entidade/propostadoacao/PropostaDoacao.java
+++ src/main/java/br/senac/helpu/modelo/entidade/propostadoacao/PropostaDoacao.java
@@ -1,18 +1,42 @@
-package br.senac.helpu.modelo.entidade.propostadoacao;
 
+package br.senac.helpu.modelo.entidade.propostadoacao;
+ 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
+ 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+ 
 import br.senac.helpu.modelo.entidade.item.Item;
 import br.senac.helpu.modelo.enumeracao.statusproposta.StatusProposta;
-
+ 
+ 
+@Entity
+@Table(name = "proposta_doacao")
+ 
 public class PropostaDoacao implements Serializable{
 	
 	private static final long serialVersionUID = 2158729290494590333L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_proposta_doacao")
 	private long id;
+	
+	
+	@Enumerated ( EnumType.STRING)
+	@Column(name = "status_proposta", length = 30, nullable = false, unique = false)
 	private StatusProposta statusProposta;
+	
+	@OneToMany(mappedBy = "proposta_doacao")
 	private List<Item> itens;
 	
 	public PropostaDoacao(long id, StatusProposta statusProposta) {
@@ -27,7 +51,7 @@ public class PropostaDoacao implements Serializable{
 	public void setId(long id) {
 		this.id = id;
 	}
-
+ 
 	public StatusProposta getStatusProposta() {
 		return statusProposta;
 	}
@@ -46,3 +70,4 @@ public class PropostaDoacao implements Serializable{
 		return serialVersionUID;
 	}
 }
+ 
