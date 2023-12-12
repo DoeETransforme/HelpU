02b5993dbb1aca8f299139a9dd8ad337794b3ae5
Pedro Krubniki Albuquerque <pedro.krubniki@gmail.com>
2023-12-12 19:21:19 -0300

    Criado o DAO do Usuario

---
+++ src/main/java/br/senac/helpu/modelo/dao/usuario/UsuarioDAO.java
@@ -0,0 +1,14 @@
+package br.senac.helpu.modelo.dao.usuario;
+
+import br.senac.helpu.modelo.entidade.usuario.Usuario;
+
+public interface UsuarioDAO {
+
+	void inserirUsuario(Usuario usuario);
+
+	void deletarUsuario(Usuario usuario);
+
+	void atualizarUsuario(Usuario usuario);
+		
+}
+
