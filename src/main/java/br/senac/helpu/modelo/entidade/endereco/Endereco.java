a6faca88f5bfbd6c70b62260310d98fc9f4a16de
IsaacTBorges <146288906+IsaacTBorges@users.noreply.github.com>
2023-12-12 14:36:00 -0300

    Alterado Mapeamento da Classe Endereço

--- src/main/java/br/senac/helpu/modelo/entidade/endereco/Endereco.java
+++ src/main/java/br/senac/helpu/modelo/entidade/endereco/Endereco.java
@@ -1,56 +1,60 @@
 package br.senac.helpu.modelo.entidade.endereco;
-
+ 
 import java.io.Serializable;
-
+ 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
+ 
 import br.senac.helpu.modelo.entidade.Ong.Ong;
-
-@Table(name = "endereco", uniqueConstraints = {@UniqueConstraint(columnNames = { })})
+ 
+@Entity
+@Table(name = "endereco")
 public class Endereco implements Serializable {
-
+ 
 	private static final long serialVersionUID = -7731215238349813452L;
-
+ 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name = "id_endereco")
 	private Long id;
-
-	@Column(name = "logadouro", length = 30, nullable = true)
+ 
+	@Column(name = "logradouro", length = 30, nullable = false)
 	private String logradouro;
-
-	@Column(name = "bairro", length = 30, nullable = true)
+ 
+	@Column(name = "bairro", length = 30, nullable = false)
 	private String bairro;
-
-	@Column(name = "complemento", length = 30, nullable = false)
+ 
+	@Column(name = "complemento", length = 30, nullable = true)
 	private String complemento;
-
-	@Column(name = "numero", nullable = true)
+ 
+	@Column(name = "numero", nullable = false)
 	private int numero;
-
-	@Column(name = "cidade", length = 30, nullable = true)
+ 
+	@Column(name = "cidade", length = 30, nullable = false)
 	private String cidade;
-	
-	@Column(name= "unidadeFederativa", length = 2, nullable = true)
+ 
+	@Column(name = "unidadeFederativa", length = 2, nullable = false)
 	private String unidadeFederativa;
-	
-	@Column(name= "cep", length = 9, nullable= false)
+ 
+	@Column(name = "cep", length = 9, nullable = false)
 	private String cep;
-	
-	@ManyToOne (fetch = FetchType.LAZY)
-			@MapsId
-			@JoinColumn(name= "id")
+ 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_ong")
 	private Ong ong;
 
+ 
+ 
+	public Endereco() {
+	}
+ 
 	public Endereco(Long id, String logradouro, String bairro, String complemento, int numero, String cidade,
 			String unidadeFederativa, String cep) {
 		setId(id);
@@ -62,7 +66,7 @@ public class Endereco implements Serializable {
 		setUnidadeFederativa(unidadeFederativa);
 		setCep(cep);
 	}
-
+ 
 	public Endereco(String logradouro, String bairro, String complemento, int numero, String cidade,
 			String unidadeFederativa, String cep) {
 		setLogradouro(logradouro);
@@ -73,69 +77,70 @@ public class Endereco implements Serializable {
 		setUnidadeFederativa(unidadeFederativa);
 		setCep(cep);
 	}
-
+ 
 	public Long getId() {
 		return id;
 	}
-
+ 
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+ 
 	public String getLogradouro() {
 		return logradouro;
 	}
-
+ 
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-
+ 
 	public String getBairro() {
 		return bairro;
 	}
-
+ 
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-
+ 
 	public String getComplemento() {
 		return complemento;
 	}
-
+ 
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
-
+ 
 	public int getNumero() {
 		return numero;
 	}
-
+ 
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-
+ 
 	public String getCidade() {
 		return cidade;
 	}
-
+ 
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-
+ 
 	public String getUnidadeFederativa() {
 		return unidadeFederativa;
 	}
-
+ 
 	public void setUnidadeFederativa(String unidadeFederativa) {
 		this.unidadeFederativa = unidadeFederativa;
 	}
-
+ 
 	public String getCep() {
 		return cep;
 	}
-
+ 
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-
+ 
 }
+
