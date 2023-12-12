a4d90efeec1e41eee256a9605cb5fb4e1882b42f
Marlon Alex Andrade <marlonalexandrade@gmail.com>
2023-12-12 16:57:34 -0300

    Alterado Pom

---
+++ src/main/java/br/senac/helpu/Main.java
@@ -0,0 +1,12 @@
+package br.senac.helpu;
+
+import java.time.LocalDate;
+
+import br.senac.helpu.modelo.entidade.alimento.Alimento;
+
+public class Main {
+	public static void main(String[] args) {
+		
+
+	}
+}
