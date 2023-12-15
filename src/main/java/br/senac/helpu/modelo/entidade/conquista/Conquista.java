94310990d0ac7a91f38d38ebf8d0b75af06a2ed9
Arthur Miranda do Carmo <tucadocarmo100578@gmail.com>
2023-12-15 17:28:13 -0300

    Atualizado o mapeamento da Classe Conquista

--- src/main/java/br/senac/helpu/modelo/entidade/conquista/Conquista.java
+++ src/main/java/br/senac/helpu/modelo/entidade/conquista/Conquista.java
@@ -2,15 +2,45 @@ package br.senac.helpu.modelo.entidade.conquista;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import br.senac.helpu.modelo.entidade.usuario.Usuario;
+
+@Entity
+@Table(name = "Conquista")
 public class Conquista implements Serializable {
 
 	private static final long serialVersionUID = 764389677409877845L;
 
+	@ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id_conquista")
 	private Long id;
+
+	@Column (name = "nome_conquista", length = 30, nullable = false, unique = true)
 	private String nome;
+
+	@Column(name = "descricao_conquista", length = 150, nullable = false, unique = true)
 	private String descricao;
 
-	public Conquista(Long id, String nome, String descricao) {
+	public Conquista() {
+	}
+
+	public Conquista(String nome, String descricao) {
+		setNome(nome);
+		setDescricao(descricao);
+	
+	}public Conquista(Long id, String nome, String descricao) {
 		setId(id);
 		setNome(nome);
 		setDescricao(descricao);
