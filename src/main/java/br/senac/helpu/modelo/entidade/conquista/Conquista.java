95304f6aad5207b4be1dd976d0691014788ddc7d
Arthur Miranda do Carmo <tucadocarmo100578@gmail.com>
2023-12-15 14:14:54 -0300

    Criada a classe Conquista

---
+++ src/main/java/br/senac/helpu/modelo/entidade/conquista/Conquista.java
@@ -0,0 +1,56 @@
+package br.senac.helpu.modelo.entidade.conquista;
+
+import java.io.Serializable;
+
+
+
+
+public class Conquista implements Serializable{
+
+	private static final long serialVersionUID = 764389677409877845L;
+
+
+	private Long id;
+	
+
+	private String nome;
+	
+
+	private String descricao;
+	
+	public Conquista () {}
+
+	public Conquista (Long id, String nome, String descricao) {
+		setId(id);
+		setNome(nome);
+		setDescricao(descricao);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
