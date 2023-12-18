06e412045496e586fdf263ca29493a57548941e1
IsaacTBorges <146288906+IsaacTBorges@users.noreply.github.com>
2023-12-18 14:16:02 -0300

    Criado o DAO da classe Conquista

---
+++ src/main/java/br/senac/helpu/modelo/dao/conquista/ConquistaDAOImpl.java
@@ -0,0 +1,146 @@
+package br.senac.helpu.modelo.dao.conquista;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+
+import br.senac.helpu.modelo.entidade.conquista.Conquista;
+import br.senac.helpu.modelo.entidade.conquista.Conquista_;
+import br.senac.helpu.modelo.factory.conexao.ConexaoFactory;
+
+public class ConquistaDAOImpl {
+	
+	private ConexaoFactory fabrica;
+ 
+	public ConquistaDAOImpl() {
+		fabrica = new ConexaoFactory();
+	}
+ 
+	public void inserirConquista(Conquista conquista) {
+		org.hibernate.Session sessao = null;
+ 
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			sessao.save(conquista);
+			sessao.getTransaction().commit();
+		} catch (Exception sqlException) {
+			sqlException.printStackTrace();
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+		} finally {
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+ 
+	public void deletarConquista(Conquista conquista) {
+		org.hibernate.Session sessao = null;
+ 
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			sessao.delete(conquista);
+			sessao.getTransaction().commit();
+		} catch (Exception sqlException) {
+			sqlException.printStackTrace();
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+		} finally {
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+ 
+	public void atualizarConquista(Conquista conquista) {
+		org.hibernate.Session sessao = null;
+ 
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			sessao.update(conquista);
+			sessao.getTransaction().commit();
+		} catch (Exception sqlException) {
+			sqlException.printStackTrace();
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+		} finally {
+			if (sessao != null) {
+				sessao.close();
+ 
+			}
+		}
+	}
+ 
+	public List<Conquista> recuperarConquista(Conquista conquista) {
+ 
+		Session sessao = null;
+		List<Conquista> conquistas = null;
+ 
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+ 
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+ 
+			CriteriaQuery<Conquista> criteria = construtor.createQuery(Conquista.class);
+			Root<Conquista> raizConquista = criteria.from(Conquista.class);
+ 
+			criteria.select(raizConquista);
+ 
+			conquistas = sessao.createQuery(criteria).getResultList();
+ 
+			sessao.getTransaction().commit();
+		} catch (Exception sqlException) {
+			sqlException.printStackTrace();
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+		} finally {
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+ 
+		return conquistas;
+ 
+	}
+ 
+	public Conquista recuperarConquistaPeloNome(String nome) {
+		Session sessao = null;
+		Conquista conquistaRecuperadoPeloNome = null;
+		try {
+ 
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+ 
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+			CriteriaQuery<Conquista> criteria = construtor.createQuery(Conquista.class);
+			Root<Conquista> raizConquista = criteria.from(Conquista.class);
+			criteria.select(raizConquista);
+			criteria.where(construtor.equal(raizConquista.get(Conquista_.nome), nome));
+			conquistaRecuperadoPeloNome = sessao.createQuery(criteria).getSingleResult();
+			sessao.getTransaction().commit();
+		} catch (Exception sqlException) {
+			sqlException.printStackTrace();
+ 
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+		} finally {
+			if (sessao != null) {
+				sessao.close();
+			}
+ 
+		}
+ 
+		return conquistaRecuperadoPeloNome;
+	}}
