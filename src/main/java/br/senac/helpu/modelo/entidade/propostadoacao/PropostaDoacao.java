0c12745f8991bc56e94628dc72319a35301fda5d
cauallr <caualucasllr@gmail.com>
2023-12-19 13:58:21 -0300

    Criado a classe PropostaDoacaoDAO

--- src/main/java/br/senac/helpu/modelo/entidade/propostadoacao/PropostaDoacao.java
+++ src/main/java/br/senac/helpu/modelo/entidade/propostadoacao/PropostaDoacao.java
@@ -1,8 +1,11 @@
 package br.senac.helpu.modelo.entidade.propostadoacao;
 
+
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -17,35 +20,56 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 
 import br.senac.helpu.modelo.entidade.doador.Doador;
 import br.senac.helpu.modelo.entidade.item.Item;
+import br.senac.helpu.modelo.entidade.ong.Ong;
 import br.senac.helpu.modelo.enumeracao.statusproposta.StatusProposta;
+
 
 @Entity
 @Table(name = "proposta_doacao")
 
+
 public class PropostaDoacao implements Serializable {
 
+
 	private static final long serialVersionUID = 2158729290494590333L;
+
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_proposta_doacao")
 	private Long id;
-		
+
+
 	@Enumerated(EnumType.STRING)
 	@Column(name = "status_proposta", length = 30, nullable = false, unique = false)
 	private StatusProposta statusProposta;
 
+
+	@Column(name = "ong", length = 45, nullable = false)
+	private Ong ong;
+	
+	@Column(name = "data_proposta", length = 10, nullable = false, unique = false)
+	private LocalDate dataCriacao;
+
+
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "propostaDoacao", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Item> itens;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_doador")
-    private Doador doador;
 
-	public PropostaDoacao() {}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_doador")
+	private Doador doador;
+
+
+	public PropostaDoacao() {
+	}
+
 
 	public PropostaDoacao(Long id, StatusProposta statusProposta) {
 		setId(id);
@@ -53,31 +77,58 @@ public class PropostaDoacao implements Serializable {
 		itens = new ArrayList<>();
 	}
 
+
 	public Long getId() {
 		return id;
 	}
+
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+
 	public StatusProposta getStatusProposta() {
 		return statusProposta;
 	}
+
 
 	public void setStatusProposta(StatusProposta statusProposta) {
 		this.statusProposta = statusProposta;
 	}
 
+
 	public void addItem(Item item) {
 		itens.add(item);
 	}
+
 
 	public void removeItem(Item item) {
 		itens.remove(item);
 	}
 
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+
+	public Ong getOng() {
+		return ong;
+	}
+
+
+	public void setOng(Ong ong) {
+		this.ong = ong;
+	}
+
+
+	public Doador getDoador() {
+		return doador;
+	}
+
+
+	public void setDoador(Doador doador) {
+		this.doador = doador;
 	}
 }
