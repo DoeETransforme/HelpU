81ff09f21be658b5730b9656e8cffa78d47135aa
Arthur Miranda do Carmo <tucadocarmo100578@gmail.com>
2023-12-21 16:43:21 -0300

    Alterado o DAO da classe Usuario

--- src/main/java/br/senac/helpu/modelo/dao/usuario/UsuarioDAO.java
+++ src/main/java/br/senac/helpu/modelo/dao/usuario/UsuarioDAO.java
@@ -1,24 +1,19 @@
 package br.senac.helpu.modelo.dao.usuario;
-<<<<<<< HEAD
- 
-import java.util.List;
- 
-=======
 
 import java.util.List;
 
->>>>>>> e733e1194781c8885f002e75e60f23c7cf501e79
 import br.senac.helpu.modelo.entidade.contato.Contato;
 import br.senac.helpu.modelo.entidade.usuario.Usuario;
- 
+
 public interface UsuarioDAO {
- 
+
 	void inserirUsuario(Usuario usuario);
- 
+
 	void deletarUsuario(Usuario usuario);
- 
+
 	void atualizarUsuario(Usuario usuario);
-		
-	List<Contato>listaContatos();
+
+	List<Contato> listaContatos();
+	
+	List<Usuario> listaUsuarios();
 }
- 
--- src/main/java/br/senac/helpu/modelo/dao/usuario/UsuarioDAOImpl.java
+++ src/main/java/br/senac/helpu/modelo/dao/usuario/UsuarioDAOImpl.java
@@ -1,15 +1,4 @@
 package br.senac.helpu.modelo.dao.usuario;
-<<<<<<< HEAD
- 
-import java.util.List;
- 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
- 
-import org.hibernate.Session;
- 
-=======
 
 import java.util.List;
 
@@ -19,119 +8,98 @@ import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 
->>>>>>> e733e1194781c8885f002e75e60f23c7cf501e79
 import br.senac.helpu.modelo.entidade.contato.Contato;
 import br.senac.helpu.modelo.entidade.usuario.Usuario;
 import br.senac.helpu.modelo.factory.conexao.ConexaoFactory;
- 
+
 public class UsuarioDAOImpl implements UsuarioDAO {
- 
+
 	private ConexaoFactory fabrica;
- 
+
 	public UsuarioDAOImpl() {
 		fabrica = new ConexaoFactory();
 	}
- 
+
 	@Override
 	public void inserirUsuario(Usuario usuario) {
 		Session sessao = null;
- 
+
 		try {
- 
+
 			sessao = fabrica.getConexao().openSession();
 			sessao.beginTransaction();
- 
+
 			sessao.save(usuario);
- 
+
 			sessao.getTransaction().commit();
- 
+
 		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
- 
+
 			if (sessao.getTransaction() != null) {
 				sessao.getTransaction().rollback();
 			}
 		}
- 
+
 	}
- 
+
 	@Override
 	public void deletarUsuario(Usuario usuario) {
 		Session sessao = null;
- 
+
 		try {
 			sessao = fabrica.getConexao().openSession();
 			sessao.beginTransaction();
- 
+
 			sessao.delete(usuario);
- 
+
 			sessao.getTransaction().commit();
- 
+
 		} catch (Exception sqlException) {
- 
+
 			sqlException.printStackTrace();
- 
+
 			if (sessao.getTransaction() != null) {
 				sessao.getTransaction().rollback();
 			}
- 
+
 		} finally {
- 
+
 			if (sessao != null) {
 				sessao.close();
 			}
 		}
- 
+
 	}
- 
+
 	@Override
 	public void atualizarUsuario(Usuario usuario) {
 		Session sessao = null;
- 
+
 		try {
- 
+
 			sessao = fabrica.getConexao().openSession();
 			sessao.beginTransaction();
- 
+
 			sessao.update(usuario);
- 
+
 			sessao.getTransaction().commit();
- 
+
 		} catch (Exception sqlException) {
- 
+
 			sqlException.printStackTrace();
- 
+
 			if (sessao.getTransaction() != null) {
 				sessao.getTransaction().rollback();
 			}
- 
+
 		} finally {
- 
+
 			if (sessao != null) {
 				sessao.close();
 			}
 		}
 	}
-<<<<<<< HEAD
- 
-	public List<Contato> listaContatos() {
-		Session sessao = null;
-		List<Contato> listaRecuperada = null;
- 
-		try {
-			sessao = fabrica.getConexao().openSession();
-			sessao.beginTransaction();
- 
-			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
- 
-			CriteriaQuery<Contato> criteria = construtor.createQuery(Contato.class);
-			Root<Contato> raizContato = criteria.from(Contato.class);
- 
-			criteria.select(raizContato);
- 
-			listaRecuperada = sessao.createQuery(criteria).getResultList();
- 
-=======
 
 	public List<Contato> listaContatos() {
 		Session sessao = null;
@@ -150,21 +118,39 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 
 			listaRecuperada = sessao.createQuery(criteria).getResultList();
 
->>>>>>> e733e1194781c8885f002e75e60f23c7cf501e79
 			sessao.getTransaction().commit();
 		} catch (Exception sqException) {
 			if (sessao.getTransaction() != null)
 				sessao.getTransaction().rollback();
 		}
 		return listaRecuperada;
-<<<<<<< HEAD
- 
-	}
- 
-}
-=======
 
 	}
 
+	public List<Usuario> listaUsuarios() {
+		Session sessao = null;
+		List<Usuario> listaRecuperada = null;
+		
+		try {
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+			
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+			
+			CriteriaQuery<Usuario>criteria = construtor.createQuery(Usuario.class);
+			Root<Usuario> raizUsuario = criteria.from(Usuario.class);
+			
+			criteria.select(raizUsuario);
+			
+			listaRecuperada = sessao.createQuery(criteria).getResultList();
+			
+			sessao.getTransaction().commit();
+		}catch(Exception sqException){
+			if(sessao.getTransaction()!= null) {
+				sessao.getTransaction().rollback();
+			}
+		}
+		return listaRecuperada;
+	}
+
 }
->>>>>>> e733e1194781c8885f002e75e60f23c7cf501e79
