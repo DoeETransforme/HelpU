1667ed646e6cc03a7f30cfb4b7dcd662ebf48294
Marlon Alex Andrade <marlonalexandrade@gmail.com>
2024-01-16 16:58:43 -0300

    Alterado Mapeamento da Classe Endereco

--- src/main/java/br/senac/helpu/modelo/entidade/endereco/Endereco.java
+++ src/main/java/br/senac/helpu/modelo/entidade/endereco/Endereco.java
@@ -1,58 +1,57 @@
 package br.senac.helpu.modelo.entidade.endereco;
-
-import java.io.Serializable;
-
-import javax.persistence.CascadeType;
+ 
+import java.io.Serializable; 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import br.senac.helpu.modelo.entidade.ong.Ong;
-import br.senac.helpu.modelo.entidade.usuario.Usuario;
-
+ 
 @Entity
 @Table(name = "endereco")
 public class Endereco implements Serializable {
-
 	private static final long serialVersionUID = -7731215238349813452L;
-
+ 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_endereco")
 	private Long id;
-
+ 
 	@Column(name = "logradouro_endereco", length = 30, nullable = false)
 	private String logradouro;
-
+ 
 	@Column(name = "bairro_endereco", length = 30, nullable = false)
 	private String bairro;
-
+ 
 	@Column(name = "numero_endereco", nullable = false)
 	private int numero;
-
+ 
 	@Column(name = "cidade_endereco", length = 30, nullable = false)
 	private String cidade;
-
+ 
 	@Column(name = "unidadeFederativa_endereco", length = 2, nullable = false)
 	private String unidadeFederativa;
-
+ 
 	@Column(name = "cep_endereco", length = 9, nullable = false)
 	private String cep;
 
 	@Column(name = "complemento_endereco", length = 30, nullable = true)
 	private String complemento;
-	
-	@MapsId
-	@OneToOne(mappedBy = "enderecos", cascade = CascadeType.ALL)
-	@JoinColumn(name = "id")
+ 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_ong", nullable = false)
 	private Ong ong;
-
+ 
 	
-	public Endereco() {
-	}
 
+	public Endereco() {}
+ 
 	public Endereco(Long id, String logradouro, String bairro, String complemento, int numero, String cidade,
 			String unidadeFederativa, String cep) {
 		setId(id);
@@ -65,8 +64,8 @@ public class Endereco implements Serializable {
 		setCep(cep);
 	}
 
-	public Endereco(Long id, String logradouro, String bairro, int numero, String cidade, String unidadeFederativa,
-			String cep) {
+	public Endereco(Long id, String logradouro, String bairro, int numero, String cidade,
+			String unidadeFederativa, String cep) {
 		setId(id);
 		setLogradouro(logradouro);
 		setBairro(bairro);
@@ -75,7 +74,7 @@ public class Endereco implements Serializable {
 		setUnidadeFederativa(unidadeFederativa);
 		setCep(cep);
 	}
-
+ 
 	public Endereco(String logradouro, String bairro, String complemento, int numero, String cidade,
 			String unidadeFederativa, String cep) {
 		setLogradouro(logradouro);
@@ -87,7 +86,8 @@ public class Endereco implements Serializable {
 		setCep(cep);
 	}
 
-	public Endereco(String logradouro, String bairro, int numero, String cidade, String unidadeFederativa, String cep) {
+	public Endereco(String logradouro, String bairro, int numero, String cidade,
+			String unidadeFederativa, String cep) {
 		setLogradouro(logradouro);
 		setBairro(bairro);
 		setNumero(numero);
@@ -95,9 +95,9 @@ public class Endereco implements Serializable {
 		setUnidadeFederativa(unidadeFederativa);
 		setCep(cep);
 	}
-	
-	
-	public Endereco(String logradouro, String bairro, int numero, String cidade, String unidadeFederativa, String cep, Ong ong ) {
+
+	public Endereco(String logradouro, String bairro, int numero, String cidade,
+			String unidadeFederativa, String cep, Ong ong) {
 		setLogradouro(logradouro);
 		setBairro(bairro);
 		setNumero(numero);
@@ -107,70 +107,71 @@ public class Endereco implements Serializable {
 		setOng(ong);
 	}
 
+ 
 	public Long getId() {
 		return id;
 	}
-
+ 
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+ 
 	public String getLogradouro() {
 		return logradouro;
 	}
-
+ 
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-
+ 
 	public String getBairro() {
 		return bairro;
 	}
-
+ 
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-
+ 
 	public String getComplemento() {
 		return complemento;
 	}
-
+ 
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
-
+ 
 	public int getNumero() {
 		return numero;
 	}
-
+ 
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-
+ 
 	public String getCidade() {
 		return cidade;
 	}
-
+ 
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-
+ 
 	public String getUnidadeFederativa() {
 		return unidadeFederativa;
 	}
-
+ 
 	public void setUnidadeFederativa(String unidadeFederativa) {
 		this.unidadeFederativa = unidadeFederativa;
 	}
-
+ 
 	public String getCep() {
 		return cep;
 	}
-
+ 
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-	
+
 	public Ong getOng() {
 		return ong;
 	}
@@ -178,5 +179,4 @@ public class Endereco implements Serializable {
 	public void setOng(Ong ong) {
 		this.ong = ong;
 	}
-
 }
--- src/main/java/br/senac/helpu/modelo/entidade/ong/Ong.java
+++ src/main/java/br/senac/helpu/modelo/entidade/ong/Ong.java
@@ -1,17 +1,13 @@
 package br.senac.helpu.modelo.entidade.ong;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
 import br.senac.helpu.modelo.entidade.contato.Contato;
 import br.senac.helpu.modelo.entidade.endereco.Endereco;
 import br.senac.helpu.modelo.entidade.pedidodoacao.PedidoDoacao;
@@ -20,7 +16,6 @@ import br.senac.helpu.modelo.entidade.usuario.Usuario;
 @Entity
 @Table(name = "ong")
 public class Ong extends Usuario implements Serializable {
-
 	private static final long serialVersionUID = -2680258071107532867L;
 
 	@Column(name = "cnpj_ong", length = 18, nullable = false, unique = true)
@@ -29,43 +24,38 @@ public class Ong extends Usuario implements Serializable {
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "ong", cascade = CascadeType.ALL)
 	private List<PedidoDoacao> pedidosdoacao;
 
-	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-	private Endereco enderecos;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "ong", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Endereco> enderecos;
 
 	public Ong() {}
 
 	public Ong(Long id, String nome, String senha, Contato contato, String cnpj) {
 		super(id, nome, senha, contato);
 		setCnpj(cnpj);
-		setEnderecos(enderecos);
-		pedidosdoacao = new ArrayList<>();
-		
 	}
 
 	public Ong(String nome, String senha, Contato contato, String cnpj) {
 		super(nome, senha, contato);
 		setCnpj(cnpj);
-		setEnderecos(enderecos);
-		pedidosdoacao = new ArrayList<>();
-		
+
 	}
 
 	public String getCnpj() {
 		return cnpj;
+
 	}
 
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
 
-	public Endereco getEnderecos() {
+	public List<Endereco> getEnderecos() {
 		return enderecos;
+
 	}
 
-	public void setEnderecos(Endereco enderecos) {
+	public void setEnderecos(List<Endereco> enderecos) {
 		this.enderecos = enderecos;
 	}
-	
-	
 
 }
