b8a5dc0b598043d0872d23f09618a53b5c6bacdd
IsaacTBorges <146288906+IsaacTBorges@users.noreply.github.com>
2024-01-23 16:20:44 -0300

    Alterado o mapeamento da classe Foto

--- src/main/java/br/senac/helpu/modelo/entidade/foto/Foto.java
+++ src/main/java/br/senac/helpu/modelo/entidade/foto/Foto.java
@@ -3,11 +3,15 @@ package br.senac.helpu.modelo.entidade.foto;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="foto")
 public class Foto implements Serializable {
 	private static final long serialVersionUID = -5786556278275043519L;
 
