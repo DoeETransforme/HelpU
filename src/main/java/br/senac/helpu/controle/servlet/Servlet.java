7f83b2cad981011a07ec1df8a506f4df714212f5
Pedro Krubniki Albuquerque <pedro.krubniki@gmail.com>
2024-01-30 14:54:29 -0300

    Criado o login

--- src/main/java/br/senac/helpu/controle/servlet/Servlet.java
+++ src/main/java/br/senac/helpu/controle/servlet/Servlet.java
@@ -28,9 +28,10 @@ import br.senac.helpu.modelo.entidade.doador.Doador;
 import br.senac.helpu.modelo.entidade.endereco.Endereco;
 import br.senac.helpu.modelo.entidade.item.Item;
 import br.senac.helpu.modelo.entidade.ong.Ong;
+import br.senac.helpu.modelo.entidade.usuario.Usuario;
 
 @WebServlet("/")
-public class Servlet extends HttpServlet{
+public class Servlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private UsuarioDAO usuarioDAO;
@@ -38,7 +39,6 @@ public class Servlet extends HttpServlet{
 	private EnderecoDAO enderecoDAO;
 	private ItemDAO itemDAO;
 	private AlimentoDAO alimentoDAO;
-
 
 	public void init() {
 		usuarioDAO = new UsuarioDAOImpl();
@@ -54,7 +54,7 @@ public class Servlet extends HttpServlet{
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
+			throws ServletException, IOException {
 
 		String action = request.getServletPath();
 
@@ -63,121 +63,125 @@ public class Servlet extends HttpServlet{
 			case "/":
 				mostrarIndex(request, response);
 				break;
-				
+
 			case "/login":
 				mostrarLogin(request, response);
 				break;
-			 	
+
+			case "/confirmar-login":
+				confirmarLogin(request, response);
+				break;
+
 			case "/perfil-doador":
 				mostrarPerfilDoador(request, response);
 				break;
-				
+
 			case "/perfil-ong":
 				mostrarPerfilOng(request, response);
 				break;
-				
+
 			case "/avaliar-proposta":
 				mostrarAvaliarProposta(request, response);
 				break;
-				
+
 			case "/confirmar-exclusao":
 				mostrarConfirmarExclusao(request, response);
 				break;
-				
+
 			case "/conta-desativada":
 				mostrarContaDesativada(request, response);
 				break;
-				
+
 			case "/descricao-pedido":
 				mostrarDescricaoPedido(request, response);
 				break;
-				
+
 			case "/editar-pedido":
 				mostrarEditarPedido(request, response);
 				break;
-				
+
 			case "/editar-perfil-ong":
 				mostrarEditarPerfilOng(request, response);
 				break;
-				
+
 			case "/editar-perfil-doador":
 				mostrarEditarPerfilDoador(request, response);
 				break;
-				
+
 			case "/excluir-conta-doador":
 				mostrarExcluirContaDoador(request, response);
 				break;
-				
+
 			case "/historico-pedidos":
 				mostrarHistoricoPedidos(request, response);
 				break;
-				
+
 			case "/historico-doacoes":
 				mostrarHistoricoDoacoes(request, response);
 				break;
-				
+
 			case "/propostas-analise":
 				mostrarPropostasAnalise(request, response);
 				break;
-				
+
 			case "/propostas-realizadas":
 				mostrarPropostasRealizadas(request, response);
 				break;
-				
+
 			case "/recuperar-senha":
 				mostrarRecuperarSenha(request, response);
 				break;
-			
+
 			case "/redefinir-senha":
 				mostrarRedefinirSenha(request, response);
 				break;
-				
+
 			case "/termos":
 				mostrarTermos(request, response);
 				break;
-							
+
 			case "/cadastro-doador":
 				mostrarCadastroDoador(request, response);
 				break;
 			case "/cadastro-ong":
 				mostrarCadastroOng(request, response);
 				break;
-				
+
 			case "/cadastro-item":
 				mostrarCadastroItem(request, response);
 				break;
-				
+
 			case "/cadastro-proposta":
 				mostrarCadastroProposta(request, response);
-				break;	
-				
+				break;
+
 //			case "/cadastro-pedido":
 //				mostrarCadastroPedido(request, response);
 //				break;
-				
+
 			case "/cadastro-alimento":
 				mostrarCadastroAlimento(request, response);
 				break;
 			case "/cadastro-ongsegundo":
 				mostrarCadastroOngP2(request, response);
 				break;
-				
+
 			case "/inserir-doador":
 				inserirDoador(request, response);
 				break;
-				
+
 			case "/inserir-ong":
-				inserirOng(request,response);
+				inserirOng(request, response);
 				break;
-				
+
 			case "/inserir-endereco-ong":
-				inserirEnderecoOng(request,response);
+				inserirEnderecoOng(request, response);
 				break;
-				
+
 			case "/inserir-item":
 				inserirItem(request, response);
 				break;
-				
+
 			case "/inserir-alimento":
 				inserirAlimento(request, response);
 				break;
@@ -196,133 +200,133 @@ public class Servlet extends HttpServlet{
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./index.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarLogin(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/login.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarPerfilDoador(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/perfil-doador.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarPerfilOng(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/perfil-ong.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarAvaliarProposta(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/avaliar-proposta.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarConfirmarExclusao(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/confirmar-exclusao.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarContaDesativada(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/conta-desativada.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarDescricaoPedido(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/descricao-pedido.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarEditarPedido(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/editar-pedido.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarEditarPerfilOng(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/editar-perfil-ong.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarEditarPerfilDoador(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/editar-perfil-doador.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarExcluirContaDoador(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/excluir-conta-doador.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarHistoricoDoacoes(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/historico-doacoes.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarHistoricoPedidos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/historico-pedidos.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarPropostasAnalise(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/propostas-analise.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarPropostasRealizadas(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/propostas-realizadas.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarRecuperarSenha(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/recuperar-senha.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarRedefinirSenha(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/redefinir-senha.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarTermos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/termos.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarCadastroDoador(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-doador.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarCadastroOng(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-ong.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarCadastroOngP2(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-ongsegundo.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarCadastroProposta(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-proposta.jsp");
@@ -334,21 +338,46 @@ public class Servlet extends HttpServlet{
 //		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-pedido.jsp");
 //		dispatcher.forward(request, response);
 //	}
-	
+
 	private void mostrarCadastroItem(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-item.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void mostrarCadastroAlimento(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./resources/paginas/cadastro-alimento.jsp");
 		dispatcher.forward(request, response);
 	}
 
+	private void confirmarLogin(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String usuarioInvalido = null;
+
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		boolean existe = usuarioDAO.verificarUsuario(email, senha);
+
+		if (existe) {
+
+			HttpSession session = request.getSession();
+			Usuario usuario = usuarioDAO.recuperarUsuarioEmail(email);
+			session.setAttribute("usuario", usuario);
+			response.sendRedirect("perfil-doador");
+		} else {
+
+			usuarioInvalido = "usuarioInvalido";
+
+			request.setAttribute("usuarioInvalido", usuarioInvalido);
+			response.sendRedirect("login");
+		}
+
+	}
+
 	private void inserirDoador(HttpServletRequest request, HttpServletResponse response)
-					throws SQLException, IOException, ServletException	{
+			throws SQLException, IOException, ServletException {
 
 		Doador doador = null;
 
@@ -356,7 +385,6 @@ public class Servlet extends HttpServlet{
 		String senha = request.getParameter("senha");
 		LocalDate data = LocalDate.parse(request.getParameter("data-nascimento"));
 		String cpf = request.getParameter("cpf");
-
 
 		Contato contato = null;
 
@@ -368,44 +396,44 @@ public class Servlet extends HttpServlet{
 
 		usuarioDAO.inserirUsuario(doador);
 		contatoDAO.inserirContato(contato);
-			
+
 		response.sendRedirect("login");
 	}
-	
-	private void inserirOng(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+
+	private void inserirOng(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
 		Ong ong = null;
 
 		String nome = request.getParameter("nome-ong");
 		String senha = request.getParameter("senha-ong");
 		String cnpj = request.getParameter("cpf-cnpj-ong");
 
-
 		Contato contato = null;
 
 		String email = request.getParameter("email-ong");
 		String telefone = request.getParameter("tefelone-ong");
-		
-		
+
 		ong = new Ong(nome, senha, cnpj);
 		contato = new Contato(telefone, email, ong);
- 
+
 		usuarioDAO.inserirUsuario(ong);
 		contatoDAO.inserirContato(contato);
-		
+
 		response.sendRedirect("cadastro-ongsegundo");
 		// No método mostrarCadastroOng
 		HttpSession session = request.getSession();
 		session.setAttribute("ong", ong);
 
 	}
-	
-	private void inserirEnderecoOng(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+
+	private void inserirEnderecoOng(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
 		// No método mostrarCadastroOngP2
 		HttpSession session = request.getSession();
 		Ong ong = (Ong) session.getAttribute("ong");
-		
+
 		Endereco endereco = null;
-			
+
 		String cidade = request.getParameter("cidade-ong");
 		String cep = request.getParameter("cep-ong");
 		String logradouro = request.getParameter("logradouro-ong");
@@ -413,45 +441,43 @@ public class Servlet extends HttpServlet{
 		String unidadeFederativa1 = request.getParameter("uf-ong");
 		String bairro = request.getParameter("bairro-ong");
 		int numero = Integer.parseInt(request.getParameter("numero-ong"));
-				
+
 		endereco = new Endereco(logradouro, bairro, numero, cidade, unidadeFederativa1, cep, complemento, ong);
 		enderecoDAO.inserirEndereco(endereco);
-		
+
 		response.sendRedirect("login");
-		
+
 	}
-	
+
 	private void inserirItem(HttpServletRequest request, HttpServletResponse response)
 			throws SQLException, IOException, ServletException {
 		HttpSession session = request.getSession();
 		Alimento alimento = (Alimento) session.getAttribute("alimento");
 		Item item = null;
-		
+
 		String quantidade = request.getParameter("quantidade");
-		
+
 		item = new Item(quantidade, alimento);
-		
+
 		itemDAO.inserirItem(item);
-		
+
 		response.sendRedirect("cadastro-proposta");
 	}
-
 
 	private void inserirAlimento(HttpServletRequest request, HttpServletResponse response)
 			throws SQLException, IOException, ServletException {
 		Alimento alimento = null;
-		
+
 		String nome = request.getParameter("nome-alimento");
 		LocalDate data = LocalDate.parse(request.getParameter("data-validade"));
-			
+
 		alimento = new Alimento(nome, data);
-		
+
 		alimentoDAO.inserirAlimento(alimento);
-		
+
 		response.sendRedirect("cadastro-item");
-		
+
 		HttpSession session = request.getSession();
 		session.setAttribute("alimento", alimento);
 	}
 }
-
