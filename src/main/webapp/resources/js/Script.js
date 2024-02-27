a8b53daf8987af350bce5165e1c6386b8f83754f
Cauã Lucas Leão Rodrigues <caualucasllr@gmail.com>
2024-02-27 11:03:56 -0300

    Alterado css

--- src/main/webapp/resources/js/Script.js
+++ src/main/webapp/resources/js/Script.js
@@ -1,31 +1,25 @@
-function toggleMenu() {
-    let menuLateral = document.getElementById("menu-lateral");
-    menuLateral.classList.toggle("mostrar");
-}
+// function toggleMenu() {
+//     let menuLateral = document.getElementById("menu-lateral");
+//     menuLateral.classList.toggle("mostrar");
+// }
 
-window.onclick = function(event) {
-    let menuLateral = document.getElementById("menu-lateral");
-    if (!event.target.matches('.hamburguer-menu') && !event.target.closest('.menu-lateral')) {
-        menuLateral.classList.remove("mostrar");
+// window.onclick = function(event) {
+//     let menuLateral = document.getElementById("menu-lateral");
+//     if (!event.target.matches('.hamburguer-menu') && !event.target.closest('.menu-lateral')) {
+//         menuLateral.classList.remove("mostrar");
+//     }
+// }
+
+
+function toggleMenuFiltro(){
+    let menuFiltro = document.getElementById("filtro-container");
+    menuFiltro.classList.toggle("mostrar1");
+}
+window.onclick = function(event){
+    let menuLateral = document.getElementById("fittro-container");
+    if(!event.target.matches('menu-topo-filtro')&& !event.target.closest('.filtro-container')){
+        menuFiltro.classList.remove("mostrar1")
     }
 }
-
-function toggleMenuFiltro() {
-    let filtro = document.getElementById("filtro-container");
-    filtro.classList.toggle("mostrar");
-}
-
-// Adicione um ouvinte de eventos para o clique na imagem SVG
-document.getElementById("imagem-filtro").addEventListener("click", toggleMenuFiltro);
-
-// Adicione um ouvinte de eventos para fechar o menu se clicar fora dele
-window.onclickFiltro = function(event) {
-    let filtro = document.getElementById("filtro-container");
-    if (!event.target.matches('#imagem-filtro') && !event.target.closest('.filtro-container')) {
-        filtro.classList.remove("mostrar");
-    }
-};
-
-
     
 
